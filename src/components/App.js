5d2667bb1b49f716f1fdd801b061a967d166e714
santosh <yadavsantosh6720@gmail.com>
2023-05-23 21:21:41 +0530

    updated count

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  let [count,setCount] = useState(0);
+
+  function updateCount() {
+    setCount(count+1);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p>Button clicked {count} times</p>
+        <button onClick={updateCount}>Click me</button>
     </div>
   )
 }
